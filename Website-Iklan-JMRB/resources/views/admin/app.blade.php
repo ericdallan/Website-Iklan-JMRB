e000e010ae5ca97d3a9daf4d0c9342e4dcd834d9
LAPTOP-GCLK9RHU\Eric Dallan <laramidia.28@gmail.com>
2022-08-03 10:24:59 +0700

    Dashboard Admin ref #8

--- Website-Iklan-JMRB/resources/views/admin/app.blade.php
+++ Website-Iklan-JMRB/resources/views/admin/app.blade.php
@@ -8,11 +8,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Document</title>
+    <title>Website-Iklan-JMRB</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
@@ -23,51 +24,50 @@
 <body>
     <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
         <a href="/dashboard" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <svg class="bi me-2" width="40" height="32">
-                <use xlink:href="#bootstrap"></use>
-            </svg>
-            <span class="fs-4">Sidebar</span>
+            <img src="{{url('Web/logowhite.png')}}">
         </a>
         <hr>
         <ul class="nav nav-pills flex-column mb-auto">
             <li class="nav-item">
-                <a href="#" class="nav-link active" aria-current="page">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#home"></use>
-                    </svg>
-                    Home
+                <a href="#" class="nav-link text-white">
+                    <i class="bi bi-pie-chart-fill" width="16" height="16"></i>
+                    Overview
                 </a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a href="#" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#speedometer2"></use>
-                    </svg>
-                    Dashboard
+                    <i class="bi bi-badge-ad-fill" width="16" height="16"></i>
+                    Iklan
                 </a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a href="#" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#table"></use>
-                    </svg>
-                    Orders
+                    <i class="fa fa-handshake-o" aria-hidden="true" width="16" height="16"></i>
+                    Negosiasi
                 </a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a href="#" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#grid"></use>
-                    </svg>
-                    Products
+                    <i class="bi bi-badge-ad-fill" width="16" height="16"></i>
+                    Payment
                 </a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a href="#" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#people-circle"></use>
-                    </svg>
-                    Customers
+                    <i class="bi bi-badge-ad-fill" width="16" height="16"></i>
+                    Reminder
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-white">
+                    <i class="bi bi-badge-ad-fill" width="16" height="16"></i>
+                    User
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-white">
+                    <i class="bi bi-badge-ad-fill" width="16" height="16"></i>
+                    Admin
                 </a>
             </li>
             <li>
