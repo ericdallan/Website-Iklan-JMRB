ca158aaee4b4d057ac3f34fcc9b6e5be47fde4c9
LAPTOP-GCLK9RHU\Eric Dallan <laramidia.28@gmail.com>
2022-08-12 15:01:01 +0700

    Perbaikan Halaman Iklan Dashboard Admin ref #16

--- Website-Iklan-JMRB/resources/views/admin/iklan.blade.php
+++ Website-Iklan-JMRB/resources/views/admin/iklan.blade.php
@@ -40,9 +40,57 @@
                 <h5 class="card-title">{{ $iklans-> name }}</h5>
                 <h6 class="card-subtitle my-2 text-muted">{{ $iklans-> zone }}</h6>
                 <h6 class="card-subtitle my-2 text-muted">{{ $iklans-> location }}</h6>
-                <p class="card-text">Total Applicant</p>
+            </div>
+            <div class="card-footer">
+                <div class="text-start">
+                    <p class="card-text text-muted">Total Applicant : </p>
+                </div>
+                <hr>
                 <div class="text-end">
-                    <a class="btn btn-default btn-sm rounded-3" href="#" role="button">Detail</a>
+                    <a class="btn btn-default btn-sm w-50 rounded-3" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $iklans->id_iklan }}">Detail</a>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal{{ $iklans->id_iklan }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Detail Iklan {{$iklans->name}}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body mx-3">
+                            <div class="row mb-3">
+                                <label for="pic_advert" class="form-label">Ganti Foto Iklan</label>
+                                <input type="file" id="pic_advert" class="form-control" name="pic_advert">
+                            </div>
+                            <div class="row mb-3">
+                                <div class="text-muted">Preview Foto Iklan</div>
+                            </div>
+                            <div class="d-flex justify-content-center align-self-center mb-3" style="height:14rem;">
+                                <img src="/Dokumen/Iklan/{{$iklans->pic_advert}}" class="img-fluid rounded" alt="">
+                            </div>
+                            <div class="row mb-2">
+                                <label for="name" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="name" placeholder="Iklan's Name" value="{{ $iklans->name }}">
+                            </div>
+                            <div class="row mb-2">
+                                <label for="zone" class="form-label">Zone</label>
+                                <input type="text" class="form-control" id="zone" placeholder="Iklan's Zone" value="{{ $iklans->zone }}">
+                            </div>
+                            <div class="row mb-2">
+                                <label for="location" class="form-label">Zone</label>
+                                <input type="text" class="form-control" id="location" placeholder="Iklan's Location" value="{{ $iklans->location }}">
+                            </div>
+                            <div class="row mb-2">
+                                <label for="rate" class="form-label">Zone</label>
+                                <input type="text" class="form-control" id="rate" placeholder="Iklan's Rate" value="{{ $iklans->rate }}">
+                            </div>
+
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -62,6 +110,7 @@
                 <form action="{{ route('dashboard/iklan/create') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Create New Advertisement</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body mx-5">
