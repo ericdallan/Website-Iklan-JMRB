e5abeadaddd233935a15ae8ee987e15b981c51b2
LAPTOP-GCLK9RHU\Eric Dallan <laramidia.28@gmail.com>
2022-08-16 13:17:18 +0700

    Model Negotiation

--- Website-Iklan-JMRB/app/Models/Negotiation.php
+++ Website-Iklan-JMRB/app/Models/Negotiation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class negotiations extends Model
+class Negotiation extends Model
 {
     use HasFactory;
 }
